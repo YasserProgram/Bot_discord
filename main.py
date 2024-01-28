dfeceba6abb2645833ec6adf236c1a3e6eb4fe25
Yasser <yassertorres2017@gmail.com>
2024-01-27 21:59:43 -0500

    Bot de discord

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+import os, random
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Ha iniciado sesión como {bot.user}')
+
+def get_duck_image_url():
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Cada vez que se llama a la solicitud de pato, el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
+@bot.command()
+async def mem(ctx):
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+ 
+    await ctx.send(file=picture)
+
+
+bot.run("")
